82c1067e311b0b12343dfb29012d1ce7c5299794
Achille AROUKO <achille.arouko@gmail.com>
2017-03-30 20:55:15 +0200

    init

---
+++ api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Hyphen Dev API",  "version": "0.1.0",  "description": "Developers API",  "title": "Platform Factory",  "url": "http://hyphen.ai/api",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-03-19T15:51:46.400Z",    "url": "http://apidocjs.com",    "version": "0.14.0"  }});
